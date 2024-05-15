2d3402701c7bd4ad81ed60e3b6a35a31197b3764
Theo PC <theobraithwaite@gmail.com>
2024-05-15 21:27:22 +1200

    Initial commit
    Connection set up (Theo's database)

---
+++ WinFrm_ByteBarApp/MainFrmByte.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WinFrm_ByteBarApp
+{
+    partial class MainFrmByte
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblByteBar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LblByteBar
+            // 
+            this.LblByteBar.AutoSize = true;
+            this.LblByteBar.Font = new System.Drawing.Font("Magneto", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblByteBar.Location = new System.Drawing.Point(264, 9);
+            this.LblByteBar.Name = "LblByteBar";
+            this.LblByteBar.Size = new System.Drawing.Size(261, 58);
+            this.LblByteBar.TabIndex = 0;
+            this.LblByteBar.Text = "Byte Bar";
+            // 
+            // MainFrmByte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblByteBar);
+            this.Name = "MainFrmByte";
+            this.Text = "Byte Bar";
+            this.Load += new System.EventHandler(this.MainFrmByte_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblByteBar;
+    }
+}
+
---
+++ WinFrm_ByteBarApp/MainFrmByte.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+
+namespace WinFrm_ByteBarApp
+{
+    public partial class MainFrmByte : Form
+    {
+        public MainFrmByte()
+        {
+            InitializeComponent();
+        }
+
+        private void MainFrmByte_Load(object sender, EventArgs e)
+        {
+            ConnectToOracle();
+        }
+
+        private static string GetConnectionString()
+        {
+            string oradb = "Data Source=oracle.cms.waikato.ac.nz:1521/teaching;User Id=tb176;Password=FmxzsURNGB;";
+            return oradb;
+        }
+
+        private void ConnectToOracle()
+        {
+            string connectionString = GetConnectionString();
+            OracleConnection conn = new OracleConnection(connectionString);
+
+            conn.Open();
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+
+            cmd.CommandText = "select * from Beer where id = 10";
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+            LblByteBar.Text = dr.GetString(0);
+            conn.Dispose();
+        }
+    }
+}
